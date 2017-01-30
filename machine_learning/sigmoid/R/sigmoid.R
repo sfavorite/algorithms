5144b401f841683368d5fbc328f2e5ef83bd5946
Scot Favorite <scot.favorite@gmail.com>
2017-01-30 12:58:43 -0500

    Updated comments in the code

--- machine_learning/sigmoid/R/sigmoid.R
+++ machine_learning/sigmoid/R/sigmoid.R
@@ -7,12 +7,14 @@ sigmoid <- function(x) {
 # Print value from sigmoid (approx 0.6125)
 print(sigmoid(0.458))
 
-# Plot x and y along sigmoid
+# Generate some 'data'
 x = seq(-10, 10, .5)
 s = {}
+
+# Compute value of sigmoid which will become our y axis
 for (i in 1:length(x)) {
-    print(i)
     s[i] <- sigmoid(x[i])
 }
-
+# Plot x and y along sigmoid
 plot(x, s)
+
