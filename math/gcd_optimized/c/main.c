2fdd76811854d95c2c6a1306ad0ea7d51bc239ab
Scot Favorite <scot.favorite@gmail.com>
2017-04-23 10:40:26 -0400

    GCD with optimization in c added

---
+++ math/gcd_optimized/c/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int gcd(int u, int v) {
+
+    int temp;
+
+    // Once u is zero we have our gcd value
+    while (u > 0) {
+        // Make sure u is larger or equal to v
+        if (u < v) {
+            temp = u;
+            u = v;
+            v = temp;
+        }
+        printf("%d % %d = %d\n", u, v, u % v);
+        // New value of u
+        u = u % v;
+    }
+    return v;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &x, &y);
+    if (x > 0 && y > 0)
+        printf("GCD of %d and %d is %d\n", x, y, gcd(x, y));
+}
