b0204aaa03933164bee58dbf64a9976587708f33
Scot Favorite <scot.favorite@gmail.com>
2017-05-20 07:51:13 -0400

    Functions needed for the list

--- double_linked_lists/c/main.c
+++ double_linked_lists/c/main.c
@@ -1,6 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node {
+    int value;
+    struct node * next;
+    struct node * prev;
+} node_t;
+
+void Initialize() {
+
+}
+
+void ListNewNode() {
+
+}
+
+void InsertAtHead() {
+
+}
+
+void InsertAtTail() {
+
+}
+
+void ListPrint() {
+
+}
+
+void ReversePrint() {
+
+}
+
+void InsertBefore() {
+
+}
+
+void InsertAfter() {
+
+}
+
+void ListDeleteNode() {
+
+}
+
+void ListDestroy() {
+}
+
 int main() {
 
     return EXIT_SUCCESS;
