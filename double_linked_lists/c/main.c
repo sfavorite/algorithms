23c931b0987b128193fdebf35a819f19fdfb338a
Scot Favorite <scot.favorite@gmail.com>
2017-05-27 09:18:27 -0400

    Added InsertAtTail function

--- double_linked_lists/c/main.c
+++ double_linked_lists/c/main.c
@@ -27,14 +27,52 @@ void InsertAtHead() {
 
 }
 
-void InsertAtTail() {
+void InsertAtTail(struct node ** tail, struct node ** head, int new_value) {
+    node_t * new_node = NULL;
+    node_t * temp = NULL;
 
+    // If the list is empty initiaize and assign new value to head
+    if ((* head) == NULL) {
+        Initialize(head, tail);
+        (* head)->value = new_value;
+    } // Add to the tail if it is NULL
+    else if ((* tail) == NULL) {
+        // Allocate memory for the tail
+        * tail = malloc(sizeof(*tail));
+
+        // Point tail prev to head
+        (* tail)->prev = * head;
+
+        // Point tail next to NULL
+        (* tail)->next = NULL;
+        
+        // Point head next to tail
+        (* head)->next = (* tail);
+
+        // Assign new data
+        (* tail)->value = new_value;
+    // Create a new node and add to the end.
+    } else {
+        // Save the current tail as temp
+        temp = (* tail);
+        // Allocate memory for the new node
+        new_node = malloc(sizeof(new_node));
+        // Set the 'old' tail next to the new node
+        temp->next = new_node;
+        // Set the values for the new_node (sooon to be tail)
+        new_node->next = NULL;
+        new_node->prev = temp;
+        (* tail) = new_node;
+        (* tail)->value = new_value;
+    }
 }
+
 
 void ListPrint(node_t * current) {
     while (current != NULL) {
         printf("%d\n", current->value);
         current = current->next;
+        printf("%p\n", current);
     }
 }
 
@@ -72,6 +110,11 @@ int main() {
     head->value = 1;
 
     ListPrint(head);
+
+    InsertAtTail(&tail, &head, 2);
+
+    ListPrint(head);
+    printf("Exit");
 
     return EXIT_SUCCESS;
 }
