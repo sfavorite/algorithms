6f64c6acbc7ef65e3f76ae17429930de38ce8070
Scot Favorite <scot.favorite@gmail.com>
2017-06-01 10:01:32 -0400

    Converted all mallocs to using node_t for size

--- double_linked_lists/c/main.c
+++ double_linked_lists/c/main.c
@@ -10,7 +10,7 @@ typedef struct node {
 // Initialize the Double Linked List
 void Initialize(struct node ** head, struct node ** tail) {
 
-    * head = malloc(sizeof(*head));
+    * head = malloc(sizeof(node_t));
 
     (* head)->next = (* tail);
     (* head)->prev = NULL;
@@ -29,42 +29,43 @@ void InsertAtHead() {
 
 void InsertAtTail(struct node ** tail, struct node ** head, int new_value) {
     node_t * new_node = NULL;
-    node_t * temp = NULL;
+     node_t * temp = NULL;
 
-    // If the list is empty initiaize and assign new value to head
-    if ((* head) == NULL) {
-        Initialize(head, tail);
-        (* head)->value = new_value;
-    } // Add to the tail if it is NULL
-    else if ((* tail) == NULL) {
-        // Allocate memory for the tail
-        * tail = malloc(sizeof(*tail));
+     // Is the list initialized?
+     if ((* head) == NULL) {
+         Initialize(head, tail);
+         (* head)->value = new_value;
+     } // Add to the tail if it is NULL
+     else if ((* tail) == NULL) {
+         // Allocate memory for the tail
+         (* tail) = malloc(sizeof(node_t));
 
-        // Point tail prev to head
-        (* tail)->prev = * head;
+         // Point tail prev to head
+         (* tail)->prev = * head;
 
-        // Point tail next to NULL
-        (* tail)->next = NULL;
+         // Point tail next to NULL
+         (* tail)->next = NULL;
 
-        // Point head next to tail
-        (* head)->next = (* tail);
+         // Point head next to tail
+         (* head)->next = (* tail);
 
-        // Assign new data
-        (* tail)->value = new_value;
-    // Create a new node and add to the end.
-    } else {
-        // Save the current tail as temp
-        temp = (* tail);
-        // Allocate memory for the new node
-        new_node = malloc(sizeof(new_node));
-        // Set the 'old' tail next to the new node
-        temp->next = new_node;
-        // Set the values for the new_node (sooon to be tail)
-        new_node->next = NULL;
-        new_node->prev = temp;
-        (* tail) = new_node;
-        (* tail)->value = new_value;
-    }
+         // Assign new data
+         (* tail)->value = new_value;
+
+     // Create a new node and add to the end.
+     } else {
+
+         // Set the current tail as temp
+         temp = (* tail);
+         // Allocate memory for the new node
+         new_node = malloc(sizeof(node_t));
+         // Set the 'old' tail next to the new node
+         temp->next = new_node;
+         new_node->next = NULL;
+         new_node->prev = temp;
+         (* tail) = new_node;
+         (* tail)->value = new_value;
+     }
 }
 
 
@@ -72,7 +73,6 @@ void ListPrint(node_t * current) {
     while (current != NULL) {
         printf("%d\n", current->value);
         current = current->next;
-        printf("%p\n", current);
     }
 }
 
