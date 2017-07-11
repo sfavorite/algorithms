855b8ba30b1bb82a55a8cb8018ca13412732cc5c
Scot Favorite <scot.favorite@gmail.com>
2017-07-11 08:52:58 -0400

    Added reverse print to node class

--- double_linked_lists_generic_data/python/dlink.py
+++ double_linked_lists_generic_data/python/dlink.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data=None, next=None, prev=None):
         self.data = data
         self.next = next
+        self.prev = prev
+
+    def printList(node):
+        while node:
+            print(node)
+            node = node.next
+
+    def reversePrint(node):
+        if node.next == None:
+            print(node)
+            return
+        node.next.reversePrint()
+        print(node)
 
     def __str__(self):
         return str(self.data)
 
 
-Hi = Node('Hi')
-One = Node(1)
-print(Hi)
-print(One)
+
+node1 = Node(1)
+node2 = Node(2)
+node1.next = node2
+node2.prev = node1
+
+
+node1.printList()
+node1.reversePrint()
